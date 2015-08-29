63844a41d61c18eab96f37f027a31fe02ee374c7
Jonah <jonah.stieglitz17@gmail.com>
2015-08-29 00:28:43 -0500

    Working on changelog/CL helper

---
+++ extras/changelogHelp/ChangelogHelper.java
@@ -0,0 +1,11 @@
+/*
+ * Created by Jonah on 8/29/2015.
+ */
+
+public class ChangelogHelper
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Change Log Help Online");
+    }
+}
